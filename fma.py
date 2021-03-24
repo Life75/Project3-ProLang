e0629236e6908f98a8781a17f86e3a1d81a8f315
Life75 <austynwashington@yahoo.com>
2021-03-24 02:01:28 -0500

    create the circles and space them out with their number values placed in them

--- fma.py
+++ fma.py
@@ -1,5 +1,10 @@
 import sys
 #TODO PARSE ARGUMENTS FOR FILEREADING INPUT AT THE END 
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+
 
 class FmaData:
     def __init__(self, amountOfStates, alphabet, stateTransisitions, startState, acceptStates):
@@ -55,6 +60,31 @@ print(fmaData.getAlphabet())
 print(fmaData.getStateTransitions())
 print(fmaData.getStartState())
 print(fmaData.getAcceptState())
+#setting up window 
+root=Tk()
+myCanvas = tkinter.Canvas(root, bg="white", height=300, width=300)
+myCanvas.create_oval(15, 25, 200, 25)
+myCanvas.pack(fill=BOTH, expand=True)
+
+
+
+
+
+class DisplayFma:
+    def __init__(self):
+        self.amountOfCircles = fmaData.getAmountOfStates
+
+       # ttk.Button(root,text="Hello Universe").grid()        
+        
+
+        
+    
+    #def displayCircles():
+
+
+display = DisplayFma()
+
+root.mainloop()
 
 
   
